fc8263d32699bee77c0594543ac9b9c29d58cb89
Prasanna Dangalla <prasannadangalla@gmail.com>
2017-11-21 14:51:46 +0530

    Modified run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
+# ----------------------------------------------------------------------------
+#  Copyright 2017 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+echo "API Manage sample business scenarios sample data populator."
+echo
+echo "1 - Managing Public, Partner vs Private APIs"
+echo "2 - Ownership, permission and collaborative API development"
+echo "7 - API Lifecycle Management"
+echo "8 - API Versioning"
+echo
 
 set -e
 
 while [ -z $sample_number ]
 do
-  echo -n "Enter the sample number (1 to 8): "
+  echo "Enter the sample number from above listed samples: "
   read -r sample_number
 done
 
-echo "This script will deploy a sample backend run the sample" . $sample_number . " scenario."
+echo "This script will deploy a sample backend run the sample"$sample_number" scenario."
 echo
 
 while [ -z $CARBON_HOME ]
 do
-  echo -n "Enter location for CARBON_HOME: "
+  echo "Enter location for CARBON_HOME: "
   read -r CARBON_HOME
 done
 
@@ -21,7 +43,7 @@ ts_path=$CARBON_HOME/repository/resources/security/client-truststore.jks
 
 while [ -z $use_default ]
 do
-  echo -n "Use Trust Store ${ts_path} (y/n): "
+  echo "Use Trust Store ${ts_path} (y/n): "
   read -r use_default
 done
 
